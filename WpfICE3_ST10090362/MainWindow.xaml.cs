8b9f13b044e8d579981a6c65a9d6c023692c1862
Pheagane23 <Pheaganer2@gmail.com>
2023-03-29 08:42:21 +0200

    Version1
    Simple Login Page

---
+++ WpfICE3_ST10090362/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//pheagane ramosa ST10090362
+//password = password and email = email@gmail.com
+namespace WpfICE3_ST10090362
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Loginbtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(passwordBOX.Password !=""&& emailtxt1.Text !="")
+            {
+                if(passwordBOX.Password=="password" && emailtxt1.Text == "email@gmail.com")
+                { MessageBox.Show("successful login!");
+                }
+            }
+        }
+
+        private void emailtxt1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
